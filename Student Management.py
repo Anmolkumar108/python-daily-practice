d9c727c13581f4dfd2c21af50752c87f1e402b68
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-08-26 22:54:44 +0530

    Add student management practice

---
+++ Student Management.py
@@ -0,0 +1,76 @@
+students = {
+    "student1": {
+        "name": "Anmol Singh",
+        "age": 19,
+        "course": "BCA",
+        "college": "SIT Aurangabad",
+        "cgpa": 8.81
+    },
+
+    "student2": {
+        "name": "Vicky Singh",
+        "age": 24,
+        "course": "B.Tech (CSE)",
+        "college": "Giani Zail Singh College of Engineering & Technology, Bathinda",
+        "cgpa": 7.8
+    },
+
+    "student3": {
+        "name": "Hritik Nayan",
+        "age": 20,
+        "course": "BCA",
+        "college": "SIT Aurangabad",
+        "cgpa": 8.1
+    }
+}
+
+
+choice = input("What information do you want? ").lower().strip()
+
+# Extra spaces ko remove karna
+choice = choice.replace("_", " ")
+
+
+# Student name identify karna
+if "student1" in choice or "student 1" in choice:
+    student = students["student1"]
+
+elif "student2" in choice or "student 2" in choice:
+    student = students["student2"]
+
+elif "student3" in choice or "student 3" in choice:
+    student = students["student3"]
+
+else:
+    print("Student not found.")
+    exit()
+
+
+# Sirf student ka naam
+if "name" in choice and "student" in choice:
+    print("Name:", student["name"])
+
+# Sirf age
+elif "age" in choice:
+    print("Age:", student["age"])
+
+# Sirf course
+elif "course" in choice:
+    print("Course:", student["course"])
+
+# Sirf college
+elif "college" in choice or "collage" in choice:
+    print("College:", student["college"])
+
+# Sirf CGPA
+elif "cgpa" in choice or "sgpa" in choice:
+    print("CGPA/SGPA:", student["cgpa"])
+
+# Agar sirf student1/student2/student3 likha
+else:
+    print("\n--- Student Information ---")
+    print("Name:", student["name"])
+    print("Age:", student["age"])
+    print("Course:", student["course"])
+    print("College:", student["college"])
+    print("CGPA/SGPA:", student["cgpa"])
