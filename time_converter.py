3e448611c00f08fa603fc466cea60b301299d1ae
Anmol Singh <akanmolsingh8@gmail.com>
2026-09-09 23:52:00 +0530

    Fix formatting of print statements in time_converter.py

--- time_converter.py
+++ time_converter.py
@@ -10,7 +10,7 @@ def convert_to_24_hour(time_str):
         return "Invalid format! Please enter the time in 'HH:MM AM/PM' format (e.g., 03:45 PM)"
 
 
-print(convert_to_24_hour("09:15 AM"))  
-print(convert_to_24_hour("03:45 PM"))  
-print(convert_to_24_hour("12:00 AM"))  
-print(convert_to_24_hour("12:30 PM"))  
+print(convert_to_24_hour("09:15 AM"))
+print(convert_to_24_hour("03:45 PM"))
+print(convert_to_24_hour("12:00 AM"))
+print(convert_to_24_hour("12:30 PM"))
